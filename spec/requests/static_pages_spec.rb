389a3af425f7917d7f55595e3fbb37ed7da5558d
Zhi Hui Tai <taizhihui@gmail.com>
2013-03-21 23:47:21 -0400

    Added Contact Us Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+	let(:base_title) {"Awesome Forum"}
+  
   describe "Home page" do
-    it "should have the content 'Forum'" do
+    it "should have the h1 'Forum'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Forum')
@@ -11,12 +14,12 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Awesome Forum | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do	
+		it "should have the h1 'Help'" do	
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
 		end
@@ -24,12 +27,12 @@ describe "Static pages" do
 		it "should have the title 'Help'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Awesome Forum | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
 	end
 
 	describe "About page" do
-		it "should have the content 'About Us'" do
+		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
 		end
@@ -37,8 +40,20 @@ describe "Static pages" do
 		it "should have the title 'About Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Awesome Forum | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
 	end
 
+	describe "Contact Us page" do
+			it "should have the h1 'Contact Us'" do
+				visit '/static_pages/contact'
+				page.should have_selector('h1', :text => 'Contact Us')
+			end
+
+			it "should have the title 'Contact Us'" do
+	      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+	      visit '/static_pages/contact'
+	      page.should have_selector('title', :text => "#{base_title} | Contact Us")
+	    end
+		end
 end
